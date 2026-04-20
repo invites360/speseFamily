9c1ba7edb1b60e09cfc44c9ad17f97d2404a7593
invites360 <giuseppetroise95@gmail.com>
2026-04-20 10:40:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "spese-family-v1";
+
+const urlsToCache = [
+  "/speseFamily/",
+  "/speseFamily/index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
